6dfefc4c7a42d11c83689ab994f20e6b698f7204
Geuniv <kkoowl99@gmail.com>
2024-11-26 14:48:29 +0900

    2024.11.26 MSA 강의 ( 이협건 교수님 - 2학년 2학기 )

---
+++ ApiGateway/src/main/java/kopo/poly/ApiGatewayApplication.java
@@ -0,0 +1,16 @@
+package kopo.poly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.reactive.config.EnableWebFlux;
+
+@EnableWebFlux
+//@EnableDiscoveryClient - Kubernetes 배포로 인해 제거
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
